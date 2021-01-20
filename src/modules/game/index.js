58b65278c55cbaa68bb80cb97ceff2eaf9e10bc9
Jonny Tullis <jon.tullis558@gmail.com>
2021-01-20 14:37:43 -0700

    Simplified module schemas

--- src/modules/game/index.js
+++ src/modules/game/index.js
@@ -3,13 +3,11 @@ const Query = require('./resolvers/query')
 const Mutation = require('./resolvers/mutation')
 const Subscription = require('./resolvers/subscription')
 
-const resolvers = {
-    Query,
-    Mutation,
-    Subscription
-}
-
 module.exports = {
     typeDefs,
-    resolvers
+    resolvers: {
+        Query,
+        Mutation,
+        Subscription
+    }
 }
--- src/modules/user/index.js
+++ src/modules/user/index.js
@@ -2,12 +2,10 @@ const typeDefs = require('./typedefs')
 const Query = require('./resolvers/query')
 const Mutation = require('./resolvers/mutation')
 
-const resolvers = {
-    Query,
-    Mutation
-}
-
 module.exports = {
     typeDefs,
-    resolvers
+    resolvers: {
+        Query,
+        Mutation
+    }
 }
