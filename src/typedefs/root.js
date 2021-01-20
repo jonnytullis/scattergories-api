48531af43e626f62c2ef54731df3a301bc3d8255
Jonny Tullis <jon.tullis558@gmail.com>
2021-01-20 10:27:23 -0700

    documentation

--- src/typedefs/root.js
+++ src/typedefs/root.js
@@ -1,3 +1,6 @@
+// This is here for the sole purpose of types Query, Mutation, and Subscriptions to be extended in other files
+// It's necessary to define these here because GraphQL will throw an error if these types are empty.
+
 const { gql } = require('apollo-server')
 
 const root = gql`
