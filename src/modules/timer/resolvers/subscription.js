95d51172427e7cd9ae6febb32b462a8c5271f294
Jonny Tullis <jon.tullis558@gmail.com>
2021-01-21 19:27:34 -0700

    Documenting comments

--- src/modules/timer/resolvers/subscription.js
+++ src/modules/timer/resolvers/subscription.js
@@ -2,7 +2,7 @@ const { ApolloError } = require('apollo-server')
 const { GameDAO } = require('../../../dao')
 
 const stoppedChannels = {} // Map of stopped channels. { channel: true } indicates the callback should stop publishing immediately
-const timerChannelCallbacks = {} // Map of open channels and their callback functions
+const timerChannelCallbacks = {} // Map of channels and their callback functions
 module.exports.timer = {
     subscribe: (_, { gameId }, { pubsub }) => {
         const game = GameDAO.get(gameId)
@@ -47,6 +47,8 @@ const getChannelFromId = (id) => `${id}_TIMER_CHANNEL`
 
 /*
  * Creates and returns a new timer
+ *      I'm creating the timer object here because it really only matters
+ *      within the context of the subscription. No timer is saved in the database.
  */
 function createTimer(seconds) {
     return {
