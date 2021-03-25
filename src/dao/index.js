d69d7598303186c5faa1ba440a9ce11df25a6070
Jonny Tullis <jon.tullis558@gmail.com>
2021-03-24 19:49:26 -0600

    Added AuthTokenDAO

--- src/dao/index.js
+++ src/dao/index.js
@@ -3,10 +3,12 @@ This export allows you to import files from this directory using this syntax:
     const { GameDAO } = require(./dao)
  */
 
+const AuthTokenDAO = require('./AuthTokenDAO')
 const GameDAO = require('./GameDAO')
 const PromptsDAO = require('./PromptsDAO')
 
 module.exports = {
+  AuthTokenDAO,
   GameDAO,
   PromptsDAO
 }
