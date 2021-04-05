ca8cff8a6f11ac768d6d00b5ffcc2b1e8cf7917d
Jonny Tullis <jon.tullis558@gmail.com>
2021-04-05 11:52:38 -0600

    Removed unnecessary code. Minor udpates

--- src/modules/timer/helpers.js
+++
@@ -1,7 +0,0 @@
-/*
- * Returns a timer subscription channel key given a gameId
- *    Use this instead of the gameId in case there is ever
- *    a different subscription type using the gameId as the
- *    channel.
- */
-module.exports.getChannel = (gameId) => `${gameId}_TIMER_CHANNEL`
