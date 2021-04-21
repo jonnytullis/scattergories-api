a760e3dfa244c4f8a743a774013559c59307135f
Jonny Tullis <jon.tullis558@gmail.com>
2021-04-21 08:30:06 -0600

    Don't automatically leave game if subscription ends (#1)

--- src/subscriptions/gameUpdated/gameUpdated.js
+++ src/subscriptions/gameUpdated/gameUpdated.js
@@ -1,7 +1,6 @@
 const { withFilter } = require('graphql-subscriptions')
 
 const gql = require('../../../gql')
-const leaveGame = require('../../mutations/leaveGame/leaveGame')
 
 const subscription = gql`
     gameUpdated(gameId: ID!): GameUpdatedResponse!
@@ -19,45 +18,18 @@ const typeDefs = gql`
     }
 `
 
-/** Adds ability to specify what the subscription does when it is canceled by the client **/
-function asyncIteratorWithCancel(asyncIterator, onCancel) {
-  const asyncReturn = asyncIterator.return
-
-  asyncIterator.return = () => {
-    onCancel()
-    return asyncReturn ? asyncReturn.call(asyncIterator) : Promise.resolve({ value: undefined, done: true })
-  }
-
-  return asyncIterator
-}
-
 const resolver = {
   gameUpdated: {
     subscribe: withFilter((_, __, context) => {
-      const { auth, pubsub, dataSources } = context
-      const { game, user } = auth.authorizeUser()
+      const { auth, pubsub } = context
+      const { game } = auth.authorizeUser()
 
       // Publish the game right away (setTimeout for nextTick)
       setTimeout(() => {
         pubsub.publish('GAME_UPDATED', { gameUpdated: { game } })
       }, 0)
 
-      async function onSubscriptionClose() {
-        try {
-          const updatedGame = await dataSources.GameDAO.getGame(game.id)
-          const updatedUser = updatedGame?.players?.find(player => player.id === user.id)
-          if (updatedUser) { // If the game and user still exist, call the leaveGame mutation
-            const res = await leaveGame.resolver.leaveGame(null, null, context)
-            if (!res.success) {
-              console.error('Error leaving game on subscription cancel')
-            }
-          }
-        } catch(e) {
-          console.error('Error in onSubscriptionClose:', e)
-        }
-      }
-
-      return asyncIteratorWithCancel(pubsub.asyncIterator([ 'GAME_UPDATED' ]), onSubscriptionClose)
+      return pubsub.asyncIterator([ 'GAME_UPDATED' ])
     },
     (payload, variables) => {
       return payload?.gameUpdated?.game?.id === variables?.gameId ||
